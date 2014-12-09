b68470001da0816d49755037a607395edd2f3de9
Steven Walter <swalter@email.unc.edu>
2014-12-09 12:53:18 -0500

    http get grabs from path

--- app/api/rest.php
+++ app/api/rest.php
@@ -1,20 +1,27 @@
 <?php 
 error_reporting(E_ALL);
+
 echo "Hi";
+
+
+
+
 require_once "orm/DBManager.php";
 DBManager::getManager() -> connect();
-$_POST = json_decode(file_get_contents('php://input'),true);
 
+$_POST = json_decode(file_get_contents('php://input'));
 
 $path = array();
-
 if(isset($_SERVER['PATH_INFO'])) {
 $path = explode('/',$_SERVER['PATH_INFO']);
 array_shift($path);
 }
-$err = "";
 
+
+
+$err = "";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
     //company registration
     if($_POST["type"] == "company"){
       $company = cleanInput($_POST["company"]);
@@ -22,20 +29,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $err .= "<br />Error -- company name not found";
         $company = null;
       }
+
       $shortname = cleanInput($_POST["shortname"]);
       if($shortname=="") {
         $shortname = $company;
       }
+
+
       $email = cleanInput($_POST["email"]);
       if($email==""){
         $err .="<br />Error -- email not found";
         $email = null;
       }
+
       $country = cleanInput($_POST["country"]);
       if($country==""){
         $err .= "<br />Error -- country not found";
         $country = null;
       }
+
       $state = cleanInput($_POST["state"]);
       if($state==""){
         $err .= "<br />Error -- state not found";
@@ -71,6 +83,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $err .= "<br />Error -- passwords do not match";
         $password = null; //Must have this so that in case of update, a null password (which is required for update) prevents update if the two don't match
       }
+
       
       if($err=="" && !isset($_POST["id"])){
         require_once "orm/Account.php";
@@ -91,7 +104,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       } else {
         if(!($err==""))
           echo $err;
+
       }
+
+
     }
     
     //Student registration
@@ -136,8 +152,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       if($err=="" && !isset($_POST["id"])){
         require_once "orm/Account.php";
         $id = Account::create('student') -> getId();
-		print_r($id);
-		
         require_once "orm/Student.php";
         Student::create($id, -1, $first_name, $last_name, $year);
         require_once "orm/Login.php";
@@ -149,10 +163,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         Student::update($id,null,$first_name,$last_name,$year);
         require_once "orm/Login.php";
         Login::update($id,$email,$password);
+
+
       } else {
         if(!($err==""))
           echo $err;
+
       }
+
       
     }
     else { //Post with incorrect type
@@ -160,6 +178,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
     
 } else if ($_SERVER["REQUEST_METHOD"] === "GET") {
+echo "hello";
     if(isset($_GET["delete"])){
       if(isset($_GET["id"])){
         require_once "orm/Account.php";
@@ -209,6 +228,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
       }
     }
+    else {
+        require_once "orm/Account.php";
+        if(Account::get($path[0], null)[0]->getType() == "student"){
+          require_once "orm/Student.php";
+          $student = Student::get($path[0],null,null,null,null)[0];
+          $arr = array('first_name' => $student->getFirst(), 'last_name' => $student->getLast(), 'year' => $student->getYear());
+          echo json_encode($arr);
+        } else {
+          require_once "orm/Company.php";
+          $company = Company::get($path[0],null,null,null,null,null,null)[0];
+          $arr = array('companyName' => $company->getName(), 'shortname' => $company->getShortName(), 'city' => $company->getCity(), 'state' => $company->getState(), 'country' => $company->getCountry(), 'website' => $company->getWebsite());
+          echo json_encode($arr);
+
+        }
+    }
+
 }
 function cleanInput($input){
     
